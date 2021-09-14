a9b3ae6ab71c0cbd46f35bdafccd69c510caf745
rokibulslame <rokibulslam.programmer@gmail.com>
2021-09-14 15:54:26 +0600

    rating color changed

--- js/app.js
+++ js/app.js
@@ -237,9 +237,9 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p class="bg-light d-inline p-1">Rating: ${product.rating.rate}</p>
+      <p class="text-danger d-inline p-1">Rating: ${product.rating.rate}</p>
       <br>
-      <p class="bg-light d-inline">${product.rating.count} Peoples Rated This</p>
+      <p class="text-danger d-inline">Total Reviews: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id}, ${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
